92f9c08e66067f259f5632d433c6abadd3591f82
Gustavobtfelix <90421959+Gustavobtfelix@users.noreply.github.com>
2022-10-07 12:36:53 -0300

    ocultando caracteres
    também mostra nome do usuario na pagina principal

--- src/telas/Login/index.js
+++ src/telas/Login/index.js
@@ -45,6 +45,7 @@ export default function Login({ navigation }) {
           autoCapitalize="none"
           value={senha}
           onChangeText={setSenha}
+          secureTextEntry={true}
         />
       </View>
 
--- src/telas/Principal/index.js
+++ src/telas/Principal/index.js
@@ -6,12 +6,14 @@ import { Feather } from 'react-native-vector-icons'
 import MaterialCommunityIcons from 'react-native-vector-icons/Feather';
 import { useContext } from 'react';
 import { TemaContext } from '../../contexts/TemaContext';
+import { AutenticacaoContext } from '../../contexts/AutenticacaoContext';
 
 
 export default function Principal({navigation}) {
   const ultimosVistos = []
 
   const { temaEscolhido } = useContext(TemaContext);
+  const { usuario } = useContext(AutenticacaoContext);
 
   const estilo = estilos(temaEscolhido)
 
@@ -19,7 +21,7 @@ export default function Principal({navigation}) {
     <View style={estilo.container}>
       <StatusBar />
       <View style={estilo.tituloArea}>
-        <Text style={estilo.titulo}>Olá, NOME</Text>
+        <Text style={estilo.titulo}>Olá, {usuario?.nome}</Text>
         <View style={estilo.carrinhoArea}>
           <TouchableOpacity onPress={() => {}}>
             <Feather name="shopping-cart" size={30} color="#fff" style={estilo.carrinhoIcon} />
