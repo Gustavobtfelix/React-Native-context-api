15ab8f7e02772bddd0dc6cdc6c5296a8239614b1
Gustavo <gustavofelix01@yahoo.com>
2022-10-08 23:11:15 -0300

    Adicionando tela de finalizar

---
+++ src/telas/Finalizar/index.js
@@ -0,0 +1,30 @@
+import { Text, View, FlatList, StatusBar, TouchableOpacity } from 'react-native';
+import { Produto } from '../../componentes/Produto';
+import { estilos } from './estilos';
+import { Feather } from 'react-native-vector-icons'
+import MaterialCommunityIcons from 'react-native-vector-icons/Feather';
+import { useContext } from 'react';
+import { TemaContext } from '../../contexts/TemaContext';
+import { AutenticacaoContext } from '../../contexts/AutenticacaoContext';
+import { ProdutosContext } from '../../contexts/ProdutosContext';
+
+
+export default function Finalizar({navigation}) {
+
+  const { temaEscolhido } = useContext(TemaContext);
+  const { usuario } = useContext(AutenticacaoContext);
+  const { quantidade, carrinho } = useContext(ProdutosContext);
+
+  const estilo = estilos(temaEscolhido)
+
+  return (
+    <View style={estilo.container}>
+      <StatusBar />
+      <TouchableOpacity style={estilo.botao}>
+        <Text strle={estilo.botaoTexto}>Finalizar Compra</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+}
+
