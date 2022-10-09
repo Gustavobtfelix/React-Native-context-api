ef3c044523f40f3d3b581356447caabb190b9ca7
Gustavo <gustavofelix01@yahoo.com>
2022-10-09 12:10:24 -0300

    Adicionando web Api
    Sendo utilizada para armazenar dados do carrinho e buscar os mesmos quando a aplicação é iniciada

--- src/contexts/ProdutosContext.js
+++ src/contexts/ProdutosContext.js
@@ -1,4 +1,6 @@
 import { createContext, useState } from 'react';
+import { useEffect } from 'react';
+import { pegarProdutos, salvarProduto } from '../servicos/requisicoes/produtos';
 
 export const ProdutosContext = createContext({});
 
@@ -7,11 +9,19 @@ export function ProdutosProvider ( {children} ) { // tudo que estiver dentro da 
     const [carrinho, setCarrinho] = useState([]);
     const [ultimosVistos, setultimosVistos] = useState([]);
 
-    function viuProduto(produto){
+    useEffect( async () => {
+        const resultado = await pegarProdutos();
+        setCarrinho(resultado);
+        setQuantidade(resultado.length);
+      }, [])
+
+    async function viuProduto(produto){
         setQuantidade(quantidade+1);
 
+        const produtoSalvo = await salvarProduto(produto)
+
         let novoCarrinho = carrinho
-        novoCarrinho.push(produto);
+        novoCarrinho.push(produtoSalvo);
         setCarrinho(novoCarrinho);
 
         let novoUltimosVistos = new Set(ultimosVistos)
